1cd8e5a1c1fa36c601dae5277dcc64f50c7b92b9
michinom <rick.jeavons@gmail.com>
2024-04-09 11:22:49 +0100

    added wait for domcontentloaded to reduce flakiness in firefox

--- tests/pages/key-press-page.ts
+++ tests/pages/key-press-page.ts
@@ -7,12 +7,17 @@ export class KeyPressPage {
         this.page = page;
     }
 
-    // Locators
+    /* Locators */
+    // This locator is private as we don't need to use it outside of this file
     private pageHeading = () => this.page.getByRole('heading', { name: 'Key Presses' });
+    // This locator is public so we can access it to make an assertion in the test
     public keyboardActionResultText = () => this.page.locator('#result');
 
-    // Methods
+    /* Methods */
     async pressKey(key : string) {
+        // This is optional to reduce chances of flakiness by waiting for the loading of the domcontent
+        await this.page.waitForLoadState('domcontentloaded');
+        
         await this.pageHeading().isVisible();
         await this.page.keyboard.press(key);
         await this.keyboardActionResultText().isVisible();
